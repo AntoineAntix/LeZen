7b84f27d046ce0b31dbbe33bbc977e24b52392c6
Tom <tramin@et.esiea.fr>
2020-04-05 16:52:52 +0200

    Mars: -Génerer des obstacles aléatoirement sur 15% de Mars

--- src/main/java/com/esiea/tp4A/Mars.java
+++ src/main/java/com/esiea/tp4A/Mars.java
@@ -1,6 +1,7 @@
 package com.esiea.tp4A;
 
 import java.util.Set;
+import java.util.Random;
 
 import com.esiea.tp4A.domain.Direction;
 import com.esiea.tp4A.domain.PlanetMap;
@@ -11,6 +12,9 @@ public class Mars implements PlanetMap {
     public final int minX;
     public final int maxY;
     public final int minY;
+    public final int tabX[] = new int[15];
+    public final int tabY[] = new int[15];
+    public final Random random = new Random();
     public Set<Position> positionObstacles;
 
     public Mars() {
@@ -22,8 +26,18 @@ public class Mars implements PlanetMap {
     }
 
     public void genererObstacles(){
-        Position obstacle = new Position.FixedPosition(5, 15, Direction.NORTH);
+        for(int i=0; i<15; i++) {
+			int randomIntP = random.nextInt(2);
+			int randomIntN = random.nextInt(2);
+			int randomInt2 = random.nextInt(51);
+			int randomInt3 = random.nextInt(51);
+			if(randomIntP == 0) randomInt2 = -randomInt2;
+			if(randomIntN == 0) randomInt3 = -randomInt3;
+			tabX[i] = randomInt2;
+			tabY[i] = randomInt3;
+        Position obstacle = new Position.FixedPosition(tabX[i], tabY[i], Direction.NORTH);
         positionObstacles.add(obstacle);
+        }
     }
 
     public Set<Position> obstaclePositions() {
