a31541a9fc4d04d330ef3c480f21ed506bc3bdb7
Mathieu Messet <messet@et.esiea.fr>
2020-04-11 19:18:58 +0200

    test création de l'objet Mars

---
+++ src/test/MarsTest.java
@@ -0,0 +1,18 @@
+package com.esiea.tp4A;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import com.esiea.tp4A.Mars;
+
+public class MarsTest{
+
+    Mars tMars = new Mars();
+
+    @Test
+    void obstacles() {
+        System.out.println("début");
+        //mars.genererObstacles();
+        //assertThat(mars.obstaclePositions().size()==15);
+        System.out.println("fin");
+    }
+}
