b901f7208251f4743bab823b69a61659a1bc0a1d
Tom <tramin@et.esiea.fr>
2020-04-12 16:05:59 +0200

    Tests sur tous les mouvements du rover effectués: -> OK

--- src/test/java/com/esiea/tp4A/RoverTest.java
+++ src/test/java/com/esiea/tp4A/RoverTest.java
@@ -15,7 +15,7 @@ public class RoverTest {
         int x = 0;
         int y = 0;
         Direction direction = Direction.NORTH;
-        char[] commande = {'f','f'};
+        char[] commande = {'f','l','b','r','b','l','f','l','f','l','f'};
         System.out.println("Test Rover en cours ...");
         System.out.println("Initialisation du laser à (0,0) avec portée de 2 ...");
         Laser laser = new Laser(x,y,mars,2);
@@ -28,9 +28,9 @@ public class RoverTest {
         System.out.println("En attente de commande ...");
         rover.move(commande);
         System.out.println("Vérifications de la position après commande ...");
-        assertThat(condMvt.getX()).isEqualTo(0);
-        assertThat(condMvt.getY()).isEqualTo(2);
-        assertThat(rMvt.gDirection()).isEqualTo(Direction.NORTH);
+        assertThat(condMvt.getX()).isEqualTo(1);
+        assertThat(condMvt.getY()).isEqualTo(-1);
+        assertThat(rMvt.gDirection()).isEqualTo(Direction.EAST);
         System.out.println("Tout est OK");
     }
 
